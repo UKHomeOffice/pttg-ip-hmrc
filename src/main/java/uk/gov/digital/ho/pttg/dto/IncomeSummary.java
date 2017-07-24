6dfaf3f3fc8974d9afa4b77ce3a31f38d4d4407f
Lucy Bennett <lucy.bennett@digital.homeoffice.gov.uk>
2017-07-24 12:59:19 +0100

    removing superclass from json dto (#5)

--- src/main/java/uk/gov/digital/ho/pttg/dto/IncomeSummary.java
+++ src/main/java/uk/gov/digital/ho/pttg/dto/IncomeSummary.java
@@ -3,14 +3,13 @@ package uk.gov.digital.ho.pttg.dto;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import org.springframework.hateoas.ResourceSupport;
 
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-public class IncomeSummary extends ResourceSupport{
+@EqualsAndHashCode
+public class IncomeSummary {
     private final List<Income> income;
     private final List<Employment> employments;
 }
