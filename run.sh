3a09c67f6c372bcec9c7a1f01ac8825f51d9ef0d
Andy Marke <andy.marke@digital.homeoffice.gov.uk>
2017-07-28 14:55:06 +0100

    no longer wait - since now use init containers in kube (#11)

--- run.sh
+++ run.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 NAME=${NAME:-pttg-ip-hmrc}
 
-while [[ ! -z ${JDK_TRUST_FILE} && ! -f ${JDK_TRUST_FILE} ]]
-do
-  echo Wating for ${JDK_TRUST_FILE}
-  sleep 2
-done
-
-sleep 10
-
 JAR=$(find . -name ${NAME}*.jar|head -1)
 java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
 
