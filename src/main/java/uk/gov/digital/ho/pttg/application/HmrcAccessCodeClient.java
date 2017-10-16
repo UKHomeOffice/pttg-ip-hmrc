50dbcb2487757622a3c1fed646cbc1840c0d0c03
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2017-10-16 16:44:12 +0100

    PTTG-492: Add Basic Auth headers for call to HMRC Access Code service.

--- src/main/java/uk/gov/digital/ho/pttg/application/HmrcAccessCodeClient.java
+++ src/main/java/uk/gov/digital/ho/pttg/application/HmrcAccessCodeClient.java
@@ -1,7 +1,6 @@
 package uk.gov.digital.ho.pttg.application;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -9,12 +8,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.digital.ho.pttg.api.RequestData;
 import uk.gov.digital.ho.pttg.dto.AuthToken;
 
 import java.time.LocalDateTime;
 
-import static uk.gov.digital.ho.pttg.api.RequestData.CORRELATION_ID_HEADER;
-import static uk.gov.digital.ho.pttg.api.RequestData.USER_ID_HEADER;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static uk.gov.digital.ho.pttg.api.RequestData.*;
 
 @Component
 @Slf4j
@@ -24,14 +24,16 @@ public class HmrcAccessCodeClient {
     private RestTemplate restTemplate;
     private final String accessCodeurl;
     private final int expiryMargin;
-
+    private final RequestData requestData;
 
     public HmrcAccessCodeClient(RestTemplate restTemplate,
                                 @Value("${base.hmrc.access.code.url}") String accessCodeurl,
-                                @Value("${access.code.expiry.margin.in.seconds}") int expiryMargin) {
+                                @Value("${access.code.expiry.margin.in.seconds}") int expiryMargin,
+                                RequestData requestData) {
         this.restTemplate = restTemplate;
         this.accessCodeurl = accessCodeurl;
         this.expiryMargin = expiryMargin;
+        this.requestData = requestData;
     }
 
     public String getAccessCode() {
@@ -53,16 +55,18 @@ public class HmrcAccessCodeClient {
     private synchronized void getAuthToken() {
         currentToken = restTemplate.exchange(accessCodeurl + "/access",
                                                 HttpMethod.GET,
-                                                createHeadersEntityWithMDC(),
+                                                generateRestHeaders(),
                                                 AuthToken.class).getBody();
     }
 
-    private HttpEntity createHeadersEntityWithMDC() {
+    private HttpEntity generateRestHeaders() {
 
         HttpHeaders headers = new HttpHeaders();
 
-        headers.add(CORRELATION_ID_HEADER, MDC.get(CORRELATION_ID_HEADER));
-        headers.add(USER_ID_HEADER, MDC.get(USER_ID_HEADER));
+        headers.add(AUTHORIZATION, requestData.hmrcBasicAuth());
+        headers.add(SESSION_ID_HEADER, requestData.sessionId());
+        headers.add(CORRELATION_ID_HEADER, requestData.correlationId());
+        headers.add(USER_ID_HEADER, requestData.userId());
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         return new HttpEntity<>("headers", headers);
--- src/test/groovy/uk/gov/digital/ho/pttg/application/HmrcAccessCodeClientTest.groovy
+++ src/test/groovy/uk/gov/digital/ho/pttg/application/HmrcAccessCodeClientTest.groovy
@@ -1,23 +1,32 @@
 package uk.gov.digital.ho.pttg.application
 
+import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
+import uk.gov.digital.ho.pttg.api.RequestData
 import uk.gov.digital.ho.pttg.dto.AuthToken
 
 import java.time.LocalDateTime
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION
+import static uk.gov.digital.ho.pttg.api.RequestData.*
+
+@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(
+        value="SE_NO_SERIALVERSIONID",
+        justification="Ignore error for serialization of closure")
 class HmrcAccessCodeClientTest extends Specification {
 
     public static final int EXPIRY_MARGIN_FOR_TEST = 99
 
     HmrcAccessCodeClient client
-    public RestTemplate mockRestTemplate = Mock(RestTemplate.class)
+    RestTemplate mockRestTemplate = Mock(RestTemplate.class)
+    RequestData mockRequestData = Mock(RequestData.class)
 
     def setup() {
-        client = new HmrcAccessCodeClient(mockRestTemplate, "some.hmrc.url", EXPIRY_MARGIN_FOR_TEST)
+        client = new HmrcAccessCodeClient(mockRestTemplate, "some.hmrc.url", EXPIRY_MARGIN_FOR_TEST, mockRequestData)
     }
 
 
@@ -94,6 +103,42 @@ class HmrcAccessCodeClientTest extends Specification {
 
             firstCall == "1234"
             secondCall == "1234"
+    }
+
+    def "should set headers for call to access code service"() {
+
+        given:
+
+            1 * mockRequestData.sessionId() >> "some session id"
+            1 * mockRequestData.correlationId() >> "some correlation id"
+            1 * mockRequestData.userId() >> "some user id"
+            1 * mockRequestData.hmrcBasicAuth() >> "some basic auth"
+
+        when:
+
+        def result = client.getAccessCode()
+
+        then:
+
+        1 * mockRestTemplate.exchange("some.hmrc.url/access",
+                                        HttpMethod.GET,
+                                        {HttpEntity httpEntity ->
+
+
+                                            httpEntity.headers.containsKey(AUTHORIZATION)
+                                            httpEntity.headers.get(AUTHORIZATION).get(0) == "some basic auth"
+                                            httpEntity.headers.containsKey(SESSION_ID_HEADER)
+                                            httpEntity.headers.get(SESSION_ID_HEADER).get(0) == "some session"
+                                            httpEntity.headers.containsKey(CORRELATION_ID_HEADER)
+                                            httpEntity.headers.get(CORRELATION_ID_HEADER).get(0) == "some correlation id"
+                                            httpEntity.headers.containsKey(USER_ID_HEADER)
+                                            httpEntity.headers.get(USER_ID_HEADER).get(0) == "some user id"
+                                        },
+                                        AuthToken.class) >>
+                new ResponseEntity<AuthToken>(new AuthToken("1234", LocalDateTime.now().plusHours(1)),
+                        HttpStatus.OK)
+
+        result == "1234"
     }
 
 }
