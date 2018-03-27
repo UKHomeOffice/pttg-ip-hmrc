f89d5c25181bfe073ffab68a9f7ad30a9538f3ee
John Evans <john.evans@bjss.com>
2018-03-27 15:04:18 +0100

    Revert "EE-For_JE: create truststore (#50)"
    This reverts commit 9a0c52546390a925854ccc754ebbecc2c5c751a2.

--- run.sh
+++ run.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-
 NAME=${NAME:-pttg-ip-hmrc}
-CACERT=/mnt/certs/local_EBSA_ROOT_CA.crt
-
-[ -f "${CACERT}" ] && keytool -import -alias ca -file "${CACERT}" -keystore /app/truststore.jks -noprompt -storepass changeit -trustcacerts
-
-keytool -importkeystore -destkeystore /app/truststore.jks -srckeystore /opt/jdk/jre/lib/security/cacerts -srcstorepass changeit -noprompt -storepass changeit &> /dev/null
 
 JAR=$(find . -name ${NAME}*.jar|head -1)
+java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
 
-java ${JAVA_OPTS} -Djavax.net.ssl.trustStore=/app/truststore.jks -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
