9ccdc85c783b22ca8bc6fb0f6c8189e7a13a01bc
John Evans <john.evans@bjss.com>
2018-07-30 12:00:17 +0100

    [EE-4657] Add log events for service start and proxy enabled^

--- src/main/java/uk/gov/digital/ho/pttg/ServiceRunner.java
+++ src/main/java/uk/gov/digital/ho/pttg/ServiceRunner.java
@@ -1,12 +1,20 @@
 package uk.gov.digital.ho.pttg;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static net.logstash.logback.argument.StructuredArguments.value;
+import static uk.gov.digital.ho.pttg.application.LogEvent.EVENT;
+import static uk.gov.digital.ho.pttg.application.LogEvent.HMRC_SERVICE_STARTED;
+
 @SpringBootApplication
+@Slf4j
 public class ServiceRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRunner.class, args);
+
+		log.info("RPS Service Started", value(EVENT, HMRC_SERVICE_STARTED));
 	}
 }
---
+++ src/main/java/uk/gov/digital/ho/pttg/application/LogEvent.java
@@ -0,0 +1,8 @@
+package uk.gov.digital.ho.pttg.application;
+
+public enum LogEvent {
+    HMRC_SERVICE_STARTED,
+    HMRC_PROXY_ENABLED;
+
+    public static final String EVENT = "event_id";
+}
