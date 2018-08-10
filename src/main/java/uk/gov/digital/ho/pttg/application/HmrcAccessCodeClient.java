6830802bf19504113327ad889c4c5c7d90699c7e
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2018-08-10 15:09:49 +0100

    Workaround for issue on jdk < 1.8.0._131
    The compiler mistakenly thinks that the exception thrown in the retry
    should be caught on jdk < 1.8.0_131.  We are catching and rethrowing the
    exception to workaround the issue.  This can be removed once we are confident
    that all jdks used in builds are on a later version.

--- src/main/java/uk/gov/digital/ho/pttg/application/HmrcAccessCodeClient.java
+++ src/main/java/uk/gov/digital/ho/pttg/application/HmrcAccessCodeClient.java
@@ -81,14 +81,17 @@ public class HmrcAccessCodeClient {
     private synchronized void getAccessCodeWithRetries() throws Exception {
         // TODO This try catch is not required on jdk >= 1.8.0_131.  Remove it once we can be sure all build and deployment jdks are up to date.
         try {
-            accessCode = Optional.of(this.retryTemplate.execute(context -> {
-                    log.info("Attempting to fetch the latest access code. Attempt number {} of {}", context.getRetryCount() + 1, maxRetryAttempts,
-                            value(EVENT, HMRC_API_CALL_ATTEMPT));
-                    return requestAccessCode();
-            }));
-        } catch(Exception e) {
-            log.info("Rethrowing following exception to workaround an issue on jdk < 1.8.0_131", e);
-            throw e;
+            accessCode = Optional.of(this.retryTemplate.execute(
+                    new RetryCallback<AccessCode, Exception>() {
+                        @Override
+                        public AccessCode doWithRetry(RetryContext context) throws Exception {
+                            log.info("Attempting to fetch the latest access code. Attempt number {} of {}", context.getRetryCount() + 1, maxRetryAttempts, value(EVENT, HMRC_API_CALL_ATTEMPT));
+                            return requestAccessCode();
+                        }
+                    }
+            ));
+        } catch (Exception ex) {
+            throw ex;
         }
 
     }
