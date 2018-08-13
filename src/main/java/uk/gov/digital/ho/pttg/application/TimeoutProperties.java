c5ea208f366cece28add3958768ad32e00e88945
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2018-08-13 11:22:04 +0100

    EE-6635 Use Lombok to tidy Configuration Properties POJOs

--- src/main/java/uk/gov/digital/ho/pttg/application/TimeoutProperties.java
+++ src/main/java/uk/gov/digital/ho/pttg/application/TimeoutProperties.java
@@ -1,100 +1,32 @@
 package uk.gov.digital.ho.pttg.application;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "timeouts")
+@NoArgsConstructor
+@Getter
+@Setter
 public class TimeoutProperties {
 
     private Audit audit;
     private HmrcApi hmrcApi;
     private HmrcAccessCode hmrcAccessCode;
 
-    public static class Audit {
+    public static class Audit extends TimeoutPropertiesTemplate {}
+    public static class HmrcApi extends TimeoutPropertiesTemplate {}
+    public static class HmrcAccessCode extends TimeoutPropertiesTemplate {}
+
+
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    private static class TimeoutPropertiesTemplate {
         private int readMs;
         private int connectMs;
-
-        public int getReadMs() {
-            return readMs;
-        }
-
-        public void setReadMs(int readMs) {
-            this.readMs = readMs;
-        }
-
-        public int getConnectMs() {
-            return connectMs;
-        }
-
-        public void setConnectMs(int connectMs) {
-            this.connectMs = connectMs;
-        }
-    }
-
-    public static class HmrcApi {
-        private int readMs;
-        private int connectMs;
-
-        public int getReadMs() {
-            return readMs;
-        }
-
-        public void setReadMs(int readMs) {
-            this.readMs = readMs;
-        }
-
-        public int getConnectMs() {
-            return connectMs;
-        }
-
-        public void setConnectMs(int connectMs) {
-            this.connectMs = connectMs;
-        }
-    }
-
-    public static class HmrcAccessCode {
-        private int readMs;
-        private int connectMs;
-
-        public int getReadMs() {
-            return readMs;
-        }
-
-        public void setReadMs(int readMs) {
-            this.readMs = readMs;
-        }
-
-        public int getConnectMs() {
-            return connectMs;
-        }
-
-        public void setConnectMs(int connectMs) {
-            this.connectMs = connectMs;
-        }
-    }
-
-    public Audit getAudit() {
-        return audit;
-    }
-
-    public void setAudit(Audit audit) {
-        this.audit = audit;
-    }
-
-    public HmrcApi getHmrcApi() {
-        return hmrcApi;
-    }
-
-    public void setHmrcApi(HmrcApi hmrcApi) {
-        this.hmrcApi = hmrcApi;
-    }
-
-    public HmrcAccessCode getHmrcAccessCode() {
-        return hmrcAccessCode;
-    }
-
-    public void setHmrcAccessCode(HmrcAccessCode hmrcAccessCode) {
-        this.hmrcAccessCode = hmrcAccessCode;
     }
 }
