c5498924811320ccaff4d90cdd1b1e102f93fb2f
Guy Sidebottom <guy.sidebottom@digital.homeoffice.gov.uk>
2018-12-17 15:28:05 +0000

    EE-10920: remove unused get request on loggin event

--- src/test/java/uk/gov/digital/ho/pttg/api/HmrcResourceTest.java
+++ src/test/java/uk/gov/digital/ho/pttg/api/HmrcResourceTest.java
@@ -105,8 +105,7 @@ public class HmrcResourceTest {
 
             return loggingEvent.getFormattedMessage().equals("Income summary successfully retrieved from HMRC") &&
                             ((ObjectAppendingMarker) loggingEvent.getArgumentArray()[0]).getFieldName().equals("event_id") &&
-                            ((ObjectAppendingMarker) loggingEvent.getArgumentArray()[1]).getFieldName().equals("request_duration_ms") &&
-                            ((ObjectAppendingMarker) loggingEvent.getArgumentArray()[2]).getFieldName().equals("thread_count");
+                            ((ObjectAppendingMarker) loggingEvent.getArgumentArray()[1]).getFieldName().equals("request_duration_ms");
         }));
     }
 }
