9c47ca788d3ee4c9b0f3e8deb13e4f2ff8faf4b4
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-17 15:22:51 +0000

    EE-12943 Fix syntax

--- update-version.sh
+++ update-version.sh
@@ -3,4 +3,4 @@ git config user.name drone
 git config user.email drone@drone
 NEW_VERSION=$(git describe --abbrev=0)
 sed "/appVersion/c\\appVersion: '${NEW_VERSION}'" Chart2.yaml > tempChart.yaml && mv tempChart.yaml Chart2.yaml
-git add . && git commit -m "Set chart version to ${NEW_VERSION} [CI SKIP]" && git push
+git add . && git commit -m "Set chart version to ${NEW_VERSION} [CI SKIP]" && git push --set-upstream origin master
