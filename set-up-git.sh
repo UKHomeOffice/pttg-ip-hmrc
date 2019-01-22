f5bd97e01de06b30d07c6f7ff2206ad4cab74bbc
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-22 15:21:57 +0000

    EE-12943 Use the app user for Git

--- set-up-git.sh
+++ set-up-git.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-mkdir fdasfdasfsa~/.ssh/ && echo "$GITHUB_SSH_KEY" > ~/.ssh/id_rsa && chmod 0600 ~/.ssh/id_rsa
-ssh-keyscan github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
-printf  "Host github.com\n   Hostname github.com\n   IdentityFile ~/.ssh/id_rsa\n" > ~/.ssh/config
-chmod 0600 ~/.ssh/config
-printf "[github]\n[user]\n user = git" > ~/.gitconfig
+mkdir /home/app/.ssh/ && echo "$GITHUB_SSH_KEY" > /home/app/.ssh/id_rsa && chmod 0600 /home/app/.ssh/id_rsa
+ssh-keyscan github.com >> /home/app/.ssh/known_hosts && chmod 600 /home/app/.ssh/known_hosts
+printf  "Host github.com\n   Hostname github.com\n   IdentityFile /home/app/.ssh/id_rsa\n" > /home/app/.ssh/config
+chmod 0600 /home/app/.ssh/config
+printf "[github]\n[user]\n user = git" > /home/app/.gitconfig
