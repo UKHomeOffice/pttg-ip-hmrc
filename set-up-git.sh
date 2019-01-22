b2e03e5bfd83b77d4242cf6877392c51fe747a2c
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-22 15:56:24 +0000

    EE-12943 Create gradle dir with correct permissions

--- set-up-git.sh
+++ set-up-git.sh
@@ -4,4 +4,4 @@ ssh-keyscan github.com >> /home/app/.ssh/known_hosts && chmod 600 /home/app/.ssh
 printf  "Host github.com\n   Hostname github.com\n   IdentityFile /home/app/.ssh/id_rsa\n" > /home/app/.ssh/config
 chmod 0600 /home/app/.ssh/config
 printf "[github]\n[user]\n user = git" > /home/app/.gitconfig
-chmod -R 0600 /drone
+
--- update-version.sh
+++ update-version.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+mkdir .gradle
+chmod 0600 .gradle
 ./gradlew release -i -s -Prelease.useAutomaticVersion=true
 NEW_VERSION=$(git describe --abbrev=0)
 sed "/appVersion/c\\appVersion: '${NEW_VERSION}'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
