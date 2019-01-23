1fdd4a5bd0e34e5b0c18596a4ac27964ea2121a8
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 15:39:37 +0000

    EE-12943 Fix typo

--- set-up-git.sh
+++ set-up-git.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir ~/.ssh/ && echo "$GITHUB_SSH_KEY" > ~/.ssh/id_rsa && chmod 0600 ~/.ssh/id_rsa
 ssh-keyscan github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
-printf  "Host github.com\n   Hostname github.com\n   IdentityFile /home/app/.ssh/id_rsa\n" > ~/.ssh/config
+printf  "Host github.com\n   Hostname github.com\n   IdentityFile ~/.ssh/id_rsa\n" > ~/.ssh/config
 chmod 0600 ~/.ssh/config
 printf "[url \"ssh://git@github.com/\"]\n insteadOf = https://github.com/\n" > ~/.gitconfig
 printf "[github]\n[user]\n user = git\n" >> ~/.gitconfig
