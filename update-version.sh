91ae18eccdc25fa92e30faf17f132cb66c617918
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 08:41:26 +0000

    EE-12943 debug

--- update-version.sh
+++ update-version.sh
@@ -3,7 +3,7 @@ pwd
 mkdir .gradle
 chmod -R 777 .
 ls -la
-./gradlew release -i -s -Prelease.useAutomaticVersion=true
+./gradlew release -i -s -Prelease.useAutomaticVersion=true --gradle-user-home=/home/app/.gradle
 NEW_VERSION=$(git describe --abbrev=0)
 sed "/appVersion/c\\appVersion: '${NEW_VERSION}'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
 git add . && git commit -m "Set chart version to ${NEW_VERSION} [CI SKIP]" && git push
