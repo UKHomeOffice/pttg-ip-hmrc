60e10ffda5c97137c82f23af93e2f623bc75a50e
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 08:07:04 +0000

    EE-12943 Try both ways of specifying gradle user home

--- update-version.sh
+++ update-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 GRADLE_USER_HOME=/home/app/gradle-user-home
-./gradlew release -i -s -Prelease.useAutomaticVersion=true
+./gradlew release -i -s -Prelease.useAutomaticVersion=true --gradle-user-home=/home/app/gradle-user-home
 NEW_VERSION=$(git describe --abbrev=0)
 sed "/appVersion/c\\appVersion: '${NEW_VERSION}'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
 git add . && git commit -m "Set chart version to ${NEW_VERSION} [CI SKIP]" && git push
