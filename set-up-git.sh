40d1255aa66c29457b18a97bbe101fdae6a32c4c
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 14:15:40 +0000

    EE-12943 debug

--- set-up-git.sh
+++ set-up-git.sh
@@ -7,3 +7,4 @@ printf "[github]\n[user]\n user = git" > ~/.gitconfig
 git config --global user.email "drone@noreply.drone.acp"
 git config --global user.name "Drone ACP"
 git checkout EE-12943-update-helm-chart
+git remote -v
