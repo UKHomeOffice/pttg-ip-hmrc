4d0d012db0dee9eedc0921a2398ab22cc34e36fb
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 09:03:40 +0000

    EE-12943 try running build as root

--- update-version.sh
+++ update-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pwd
 ls -la
-./gradlew release -i -s -Prelease.useAutomaticVersion=true --gradle-user-home=/tmp
+./gradlew release -i -s -Prelease.useAutomaticVersion=true
 NEW_VERSION=$(git describe --abbrev=0)
 sed "/appVersion/c\\appVersion: '${NEW_VERSION}'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
 git add . && git commit -m "Set chart version to ${NEW_VERSION} [CI SKIP]" && git push
