44549b08dd16b9d3fa590ba8e3c527e7932516f1
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 14:25:20 +0000

    EE-12943 Force ssh on remotes

--- set-up-git.sh
+++ set-up-git.sh
@@ -4,7 +4,7 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
 printf  "Host github.com\n   Hostname github.com\n   IdentityFile /home/app/.ssh/id_rsa\n" > ~/.ssh/config
 chmod 0600 ~/.ssh/config
 printf "[github]\n[user]\n user = git" > ~/.gitconfig
+printf "[url "ssh://git@github.com/"]\n  insteadOf = https://github.com/"
 git config --global user.email "drone@noreply.drone.acp"
 git config --global user.name "Drone ACP"
 git checkout EE-12943-update-helm-chart
-git remote -v
