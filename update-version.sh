e3548d7c434aaeabd59745841e6e7a8969afbea6
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 08:11:27 +0000

    EE-12943 Create gradle directory up front

--- update-version.sh
+++ update-version.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-GRADLE_USER_HOME=/home/app/gradle-user-home
+mkdir /home/app/gradle-user-home
+chmod 0600 /home/app/.gradle
 ./gradlew release -i -s -Prelease.useAutomaticVersion=true --gradle-user-home=/home/app/gradle-user-home
 NEW_VERSION=$(git describe --abbrev=0)
 sed "/appVersion/c\\appVersion: '${NEW_VERSION}'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
