796c6a7109f3917b3a4c42303d7d22de326c4d5a
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 08:39:56 +0000

    EE-12943 debug

--- update-version.sh
+++ update-version.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-ls -la
 pwd
 mkdir .gradle
 chmod -R 777 .
+ls -la
 ./gradlew release -i -s -Prelease.useAutomaticVersion=true
 NEW_VERSION=$(git describe --abbrev=0)
 sed "/appVersion/c\\appVersion: '${NEW_VERSION}'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
