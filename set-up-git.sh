373dfb72747f053799582c9849af5f60d2499162
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 15:25:31 +0000

    EE-12943 debugging

--- set-up-git.sh
+++ set-up-git.sh
@@ -5,6 +5,7 @@ printf  "Host github.com\n   Hostname github.com\n   IdentityFile /home/app/.ssh
 chmod 0600 ~/.ssh/config
 printf "[github]\n[user]\n user = git" > ~/.gitconfig
 printf "[url "ssh://git@github.com/"]\n  insteadOf = https://github.com/" >> ~/.gitconfig
+cat ~/.gitconfig
 git config --global user.email "drone@noreply.drone.acp"
 git config --global user.name "Drone ACP"
 git checkout EE-12943-update-helm-chart
