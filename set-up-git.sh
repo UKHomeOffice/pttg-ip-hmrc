22fd073dffbad819867fe655b37e9eb76983cefd
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 09:14:12 +0000

    EE-12943 use branch which is allowed to call release plugin

--- set-up-git.sh
+++ set-up-git.sh
@@ -4,3 +4,4 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
 printf  "Host github.com\n   Hostname github.com\n   IdentityFile /home/app/.ssh/id_rsa\n" > ~/.ssh/config
 chmod 0600 ~/.ssh/config
 printf "[github]\n[user]\n user = git" > ~/.gitconfig
+git checkout EE-12943-update-helm-chart
