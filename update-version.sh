081747a6c53bc0a14778f8ae44c193e3d34552a5
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 08:30:57 +0000

    EE-12943 move git library into current directory

--- update-version.sh
+++ update-version.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 ls -la
-cp -R .gradle /home/app
+pwd
+mkdir /home/app/.gradle
 chmod 0600 /home/app/.gradle
+cp .git /home/app/.git
 ./gradlew release -i -s -Prelease.useAutomaticVersion=true --gradle-user-home=/home/app/.gradle
 NEW_VERSION=$(git describe --abbrev=0)
 sed "/appVersion/c\\appVersion: '${NEW_VERSION}'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
