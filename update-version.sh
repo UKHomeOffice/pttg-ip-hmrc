c4370ef1da32619d6f0dc1e73053078bc3ee7835
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 08:25:04 +0000

    EE-12943 fix typo

--- update-version.sh
+++ update-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir /home/app/.gradle
 chmod 0600 /home/app/.gradle
-./gradlew release -i -s -Prelease.useAutomaticVersion=true --gradle-user-home=/home/app/gradle-user-home
+./gradlew release -i -s -Prelease.useAutomaticVersion=true --gradle-user-home=/home/app/.gradle
 NEW_VERSION=$(git describe --abbrev=0)
 sed "/appVersion/c\\appVersion: '${NEW_VERSION}'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
 git add . && git commit -m "Set chart version to ${NEW_VERSION} [CI SKIP]" && git push
