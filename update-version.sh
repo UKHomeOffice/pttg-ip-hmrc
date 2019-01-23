28940205811230ec6a8ccbbba4172ba08b34b005
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 08:35:24 +0000

    EE-12943 try giving full permissions to current directory

--- update-version.sh
+++ update-version.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 ls -la
 pwd
-mkdir /home/app/.gradle
-chmod 0600 /home/app/.gradle
-cp .git /home/app/.git
-./gradlew release -i -s -Prelease.useAutomaticVersion=true --gradle-user-home=/home/app/.gradle
+chmod -R 777 .
+./gradlew release -i -s -Prelease.useAutomaticVersion=true
 NEW_VERSION=$(git describe --abbrev=0)
 sed "/appVersion/c\\appVersion: '${NEW_VERSION}'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
 git add . && git commit -m "Set chart version to ${NEW_VERSION} [CI SKIP]" && git push
