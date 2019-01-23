9223248d16d5bc04b44e349445a2df20cfd01d7f
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-23 15:36:50 +0000

    EE-12943 Force ssh on remotes

--- set-up-git.sh
+++ set-up-git.sh
@@ -3,8 +3,8 @@ mkdir ~/.ssh/ && echo "$GITHUB_SSH_KEY" > ~/.ssh/id_rsa && chmod 0600 ~/.ssh/id_
 ssh-keyscan github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
 printf  "Host github.com\n   Hostname github.com\n   IdentityFile /home/app/.ssh/id_rsa\n" > ~/.ssh/config
 chmod 0600 ~/.ssh/config
-printf "[github]\n[user]\n user = git\n" > ~/.gitconfig
-printf "[url "ssh://git@github.com/"]\n  insteadOf = https://github.com/\n" >> ~/.gitconfig
+printf "[url \"ssh://git@github.com/\"]\n insteadOf = https://github.com/\n" > ~/.gitconfig
+printf "[github]\n[user]\n user = git\n" >> ~/.gitconfig
 cat ~/.gitconfig
 git config --global user.email "drone@noreply.drone.acp"
 git config --global user.name "Drone ACP"
