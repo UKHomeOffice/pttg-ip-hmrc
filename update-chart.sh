2b6a278ec3fbf7e812b8646c66b65a7ba2a5f00a
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-25 15:37:07 +0000

    EE-12943 Move update chart script into own file

---
+++ update-chart.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+NEW_VERSION=$(sed -e 's/^version=//' -e 's/-SNAPSHOT//' gradle.properties)
+sed "/appVersion/c\\appVersion: '$NEW_VERSION'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
+git add Chart.yaml
