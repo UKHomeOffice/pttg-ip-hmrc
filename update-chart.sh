99d7b21c0aa0f32763bb9a90b36db9ea5e1c6364
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-01-29 08:58:52 +0000

    EE-12943 Bash is installed on the build container so use it

--- update-chart.sh
+++ update-chart.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!usr/bin/env bash
 set -e
 NEW_VERSION=$(sed -e 's/^version=//' -e 's/-SNAPSHOT//' gradle.properties)
 sed "/appVersion/c\\appVersion: '$NEW_VERSION'" Chart.yaml > tempChart.yaml && mv tempChart.yaml Chart.yaml
