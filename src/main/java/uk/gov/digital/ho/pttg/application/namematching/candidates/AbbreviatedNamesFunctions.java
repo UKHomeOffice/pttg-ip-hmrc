01569df0627629fad884328a095e1d50944cad48
Owain Roberts <owain.roberts@bjss.com>
2019-02-11 13:53:25 +0000

    Revert "Merge pull request #172 from UKHomeOffice/revert-171-EE-10741"
    This reverts commit 68488ce940e455931e9902e799253bbf4dfc8b73, reversing
    changes made to 7ecae706bac363fecf83f4222187d45e2837db64.

--- src/main/java/uk/gov/digital/ho/pttg/application/namematching/candidates/AbbreviatedNamesFunctions.java
+++ src/main/java/uk/gov/digital/ho/pttg/application/namematching/candidates/AbbreviatedNamesFunctions.java
@@ -12,25 +12,26 @@ import static java.util.Collections.emptyList;
 public final class AbbreviatedNamesFunctions {
 
     private static final String ANY_LETTER_INCLUDING_UNICODE_MATCHER = "\\p{L}\\p{M}*+";
-    private static final String FULL_STOP_SPACE_MATCHER = "\\.\\s+";
-    private static final String FULL_STOP_SPACE_BETWEEN_NAMES_PATTERN = ANY_LETTER_INCLUDING_UNICODE_MATCHER + FULL_STOP_SPACE_MATCHER + ANY_LETTER_INCLUDING_UNICODE_MATCHER;
-    private static final Pattern FULL_STOP_SPACE_PATTERN_REGEX = Pattern.compile(FULL_STOP_SPACE_BETWEEN_NAMES_PATTERN);
+    private static final String ABBREVIATION_SPACE_MATCHER = "[.']\\s+";
 
-    private static final String FULL_STOP_SPACE_NEGATIVE_LOOK_BEHIND = "(?<!(\\.|\\s))";
-    private static final String SPACE_NOT_PRECEDED_BY_FULL_STOP_OR_SPACE_PATTERN = FULL_STOP_SPACE_NEGATIVE_LOOK_BEHIND + "\\s+";
+    private static final String ABBREVIATION_SPACE_BETWEEN_NAMES_PATTERN = ANY_LETTER_INCLUDING_UNICODE_MATCHER + ABBREVIATION_SPACE_MATCHER + ANY_LETTER_INCLUDING_UNICODE_MATCHER;
+    private static final Pattern ABBREVIATION_SPACE_REGEX_PATTERN = Pattern.compile(ABBREVIATION_SPACE_BETWEEN_NAMES_PATTERN);
+
+    private static final String ABBREVIATION_SPACE_NEGATIVE_LOOK_BEHIND = "(?<!(\\.|'|\\s))";
+    private static final String SPACE_NOT_PRECEDED_BY_ABBREVIATION_OR_SPACE_PATTERN = ABBREVIATION_SPACE_NEGATIVE_LOOK_BEHIND + "\\s+";
 
     static boolean doesNotContainAbbreviatedNames(InputNames inputNames) {
-        if (nameContainsFullStopSpaceBetweenNames(inputNames.fullFirstName())) {
+        if (nameContainsAbbreviationSpaceBetweenNames(inputNames.fullFirstName())) {
             return false;
         }
-        if (nameContainsFullStopSpaceBetweenNames(inputNames.fullLastName())) {
+        if (nameContainsAbbreviationSpaceBetweenNames(inputNames.fullLastName())) {
             return false;
         }
-        return !nameContainsFullStopSpaceBetweenNames(inputNames.fullAliasNames());
+        return !nameContainsAbbreviationSpaceBetweenNames(inputNames.fullAliasNames());
     }
 
-    private static boolean nameContainsFullStopSpaceBetweenNames(String name) {
-        return FULL_STOP_SPACE_PATTERN_REGEX.matcher(name).find();
+    private static boolean nameContainsAbbreviationSpaceBetweenNames(String name) {
+        return ABBREVIATION_SPACE_REGEX_PATTERN.matcher(name).find();
     }
 
     public static List<String> splitAroundAbbreviatedNames(String names) {
@@ -39,7 +40,7 @@ public final class AbbreviatedNamesFunctions {
             return emptyList();
         }
 
-        String[] splitNames = names.split(SPACE_NOT_PRECEDED_BY_FULL_STOP_OR_SPACE_PATTERN);
+        String[] splitNames = names.split(SPACE_NOT_PRECEDED_BY_ABBREVIATION_OR_SPACE_PATTERN);
 
         return Arrays.stream(splitNames)
                 .map(AbbreviatedNamesFunctions::removeMultipleSpaces)
