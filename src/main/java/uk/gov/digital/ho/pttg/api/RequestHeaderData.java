2ad9bb7ce58351a86779fba23900aeb9c6180e7b
Owain Roberts <owain.roberts@bjss.com>
2019-08-27 12:47:33 +0100

    EE-6825 Fixing checkstyle violation for too many public methods by making new isASmokeTest() method package-private

--- src/main/java/uk/gov/digital/ho/pttg/api/RequestHeaderData.java
+++ src/main/java/uk/gov/digital/ho/pttg/api/RequestHeaderData.java
@@ -220,7 +220,7 @@ public class RequestHeaderData implements HandlerInterceptor {
         }
     }
 
-    public boolean isASmokeTest() {
+    boolean isASmokeTest() {
         return userId().equals(SMOKE_TESTS_USER_ID);
     }
 }
