0e58f939ea559e0c7176088c8f9b33bb92718cd7
Tim-Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2020-05-27 19:56:41 +0100

    EE-32216: Reinstate the truststore

--- run.sh
+++ run.sh
@@ -17,12 +17,12 @@ else
     rm "${file}"
   done
 
-#   certfiles=$(awk '/-----BEGIN CERTIFICATE-----/{filename="acpca-b-"NR; print filename}; {print >filename}' /certs/ca-certificates.crt)
+  certfiles=$(awk '/-----BEGIN CERTIFICATE-----/{filename="acpca-b-"NR; print filename}; {print >filename}' /certs/ca-certificates.crt)
 
-#   for file in ${certfiles}; do
-#     keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts
-#     rm "${file}"
-#   done
+  for file in ${certfiles}; do
+    keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts
+    rm "${file}"
+  done
 
   java ${JAVA_OPTS} -Djavax.net.ssl.trustStore=/app/truststore.jks \
                   -Dcom.sun.management.jmxremote.local.only=false \
