37a8f187ad82c8a741668f92554bde9f2ca2c7a9
Tim-Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2020-05-27 21:16:12 +0100

    EE-32216: Add CAs from static directory (#236)

--- run.sh
+++ run.sh
@@ -17,21 +17,9 @@ else
     rm "${file}"
   done
 
-#   certfiles=$(awk '/-----BEGIN CERTIFICATE-----/{filename="acpca-b-"NR; print filename}; {print >filename}' /certs/ca-certificates.crt)
-
-#   for file in ${certfiles}; do
-#     keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts
-#     rm "${file}"
-#   done
-
-  awk '/-----BEGIN CERTIFICATE-----/{filename="acpca-b-"NR; print filename}; {print >filename}' /certs/ca-certificates.crt
-  
-  for file in acpca-b*; do
-    echo "Process certificate in ${file}"
-    keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts
-    echo "Now remove cert file ${file}"
-    rm "${file}"
-  done  
+   for file in ./DigiCert*; do
+     keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts
+   done
 
   java ${JAVA_OPTS} -Djavax.net.ssl.trustStore=/app/truststore.jks \
                   -Dcom.sun.management.jmxremote.local.only=false \
