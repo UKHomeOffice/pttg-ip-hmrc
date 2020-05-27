f333636e31d08009e35fb0c441828a4918cf2f77
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2020-05-27 17:58:52 +0100

    EE-32216: Add CAs from the file in the volume linked to the bundle configmap

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ if [ -z ${ACP_MODE} ]; then
 else
   echo "ACP Deployment"
 
-  certfiles=$(awk '/-----BEGIN CERTIFICATE-----/{filename="acpca"NR; print filename}; {print >filename}' /certs/acp-root.crt)
+  certfiles=$(awk '/-----BEGIN CERTIFICATE-----/{filename="acpca-a-"NR; print filename}; {print >filename}' /certs/acp-root.crt)
 
   for file in ${certfiles}
   do
@@ -17,7 +17,12 @@ else
     rm "${file}"
   done
 
-  keytool -importkeystore -destkeystore /app/truststore.jks -srckeystore /opt/jdk/jre/lib/security/cacerts -srcstorepass changeit -noprompt -storepass changeit &> /dev/null
+  certfiles=$(awk '/-----BEGIN CERTIFICATE-----/{filename="acpca-b-"NR; print filename}; {print >filename}' /certs/ca-certificates.crt)
+
+  for file in ${certfiles}; do
+    keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts
+    rm "${file}"
+  done
 
   java ${JAVA_OPTS} -Djavax.net.ssl.trustStore=/app/truststore.jks \
                   -Dcom.sun.management.jmxremote.local.only=false \
