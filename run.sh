ea1230208d5c88f35d68e4fd3d8dfe1127e0bfc0
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2020-05-28 10:48:47 +0100

    EE-32216: Add debug info

--- run.sh
+++ run.sh
@@ -11,18 +11,28 @@ else
 
   certfiles=$(awk '/-----BEGIN CERTIFICATE-----/{filename="acpca-a-"NR; print filename}; {print >filename}' /certs/acp-root.crt)
 
-  for file in ${certfiles}
-  do
-    keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts
+  echo 'Add the ACP Root CA certs'
+
+  for file in ${certfiles}; do
+    echo -n '.'
+    keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts > /dev/null
     rm "${file}"
   done
 
   certfiles=$(awk '/-----BEGIN CERTIFICATE-----/{filename="acpca-b-"NR; print filename}; {print >filename}' /certs/ca-certificates.crt)
 
+  echo 'Now add the Commercial Root CA certs'
+
+  cacount=0
+
   for file in ${certfiles}; do
-    keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts
+    echo -n '.'
+    keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts 2> /dev/null
     rm "${file}"
+    let "cacount++"
   done
+
+  echo "Trustore has been created with ${cacount} certificates, now start the app ... "
 
   java ${JAVA_OPTS} -Djavax.net.ssl.trustStore=/app/truststore.jks \
                   -Dcom.sun.management.jmxremote.local.only=false \
