dc873512d10dc23f75b998d873d9443fede72131
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2020-05-28 09:41:51 +0100

    EE-32216: Demonstrate the deployment problem

--- run.sh
+++ run.sh
@@ -17,9 +17,12 @@ else
     rm "${file}"
   done
 
-   for file in ./DigiCert*; do
-     keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts
-   done
+  certfiles=$(awk '/-----BEGIN CERTIFICATE-----/{filename="acpca-b-"NR; print filename}; {print >filename}' /certs/ca-certificates.crt)
+
+  for file in ${certfiles}; do
+    keytool -import -alias "${file}" -file "${file}" -keystore ./truststore.jks -noprompt -storepass changeit -trustcacerts
+    rm "${file}"
+  done
 
   java ${JAVA_OPTS} -Djavax.net.ssl.trustStore=/app/truststore.jks \
                   -Dcom.sun.management.jmxremote.local.only=false \
